ffb630ea23e2b475cc3840bc26199160fbe7dab2
khanm2758 <khanm2758@gmail.com>
2025-03-14 05:24:13 +0600

    setting up repository name

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/safeNetDefender/", // Add this line
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // ✅ Define alias for "src"
